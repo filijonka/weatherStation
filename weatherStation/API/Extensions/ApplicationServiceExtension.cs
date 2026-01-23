e297e464bc5395c9d3fe61f9bc5ac711bb7bf29e
Peter Larsson <info@letitrock.se>
2026-01-23 10:45:55 +0100

    Added a createlogger method and added two new services

--- weatherStation/API/Extensions/ApplicationServiceExtension.cs
+++ weatherStation/API/Extensions/ApplicationServiceExtension.cs
@@ -1,10 +1,14 @@
+using API.Auth.Netatmo.Interface;
+using API.Auth.Netatmo.Options;
+using API.Auth.Netatmo.Services;
 using API.Interface;
 using API.Options;
 using API.Services;
+using Persistance.Influx;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
-using WeatherStation.Persistence.Influx;
 
 namespace API.Extensions;
 
@@ -16,23 +20,40 @@ public static class ApplicationServiceExtension
     /// <summary>
     /// Adds application services for this API.
     /// </summary>
-    /// <param name="services"></param>
-    /// <param name="configuration"></param>
-    /// <returns>IServiceCollection</returns>
+    /// <param name="services">Service collection to add registrations to.</param>
+    /// <param name="configuration">Application configuration.</param>
+    /// <returns>Service collection for chaining.</returns>
     public static IServiceCollection AddApplicationServices(
         this IServiceCollection services,
         IConfiguration configuration
     )
     {
-        services.AddOpenApi();
         services.Configure<InfluxOptions>(configuration.GetSection("Influx"));
         services.Configure<WeatherApiOptions>(configuration.GetSection("WeatherApi"));
-        services.AddSingleton<Serilog.ILogger>(_ => Log.Logger);
+        services.Configure<NetatmoOptions>(configuration.GetSection("Netatmo"));
         services.AddSingleton<IInfluxClientFactory, InfluxClientFactory>();
         services.AddSingleton<IInfluxWriteService, InfluxWriteService>();
         services.AddHttpClient<IExternalWeatherClient, ExternalWeatherClient>();
         services.AddSingleton<IWeatherIngestService, WeatherIngestService>();
+        services.AddSingleton<INetatmoTokenStore, NetatmoTokenStore>();
+        services.AddHttpClient<INetatmoOAuthClient, NetatmoOAuthClient>();
 
         return services;
+    }
+
+    /// <summary>
+    /// Creates the Serilog logger instance.
+    /// </summary>
+    /// <param name="configuration">Application configuration.</param>
+    /// <returns>Configured Serilog logger instance.</returns>
+    public static ILogger CreateLogger(IConfiguration configuration)
+    {
+        LoggerConfiguration loggerConfiguration = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .Enrich.FromLogContext();
+
+        ILogger logger = loggerConfiguration.CreateLogger();
+
+        return logger;
     }
 }
