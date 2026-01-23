49ddb0219bcdf9ec9b6c3afe6b6af05e02b446b9
Peter Larsson <info@letitrock.se>
2026-01-23 12:26:04 +0100

    REmoved unused services and code

--- weatherStation/API/Services/WeatherIngestService.cs
+++ weatherStation/API/Services/WeatherIngestService.cs
@@ -1,33 +1,36 @@
+using System;
 using API.Interface;
-using WeatherStation.Persistence.Influx;
-using WeatherStation.Persistence.Models;
-using ILogger = Serilog.ILogger;
+using Persistance.Influx;
+
+using Serilog;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace API.Services;
 
+/// <inheritdoc />
 public sealed class WeatherIngestService : IWeatherIngestService
 {
-    private readonly IExternalWeatherClient externalWeatherClient;
     private readonly IInfluxWriteService influxWriteService;
     private readonly ILogger logger;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WeatherIngestService"/> class.
+    /// </summary>
+    /// <param name="influxWriteService">Influx write service.</param>
+    /// <param name="logger">Serilog logger.</param>
     public WeatherIngestService(
-        IExternalWeatherClient externalWeatherClient,
         IInfluxWriteService influxWriteService,
         ILogger logger
     )
     {
-        this.externalWeatherClient = externalWeatherClient;
         this.influxWriteService = influxWriteService;
         this.logger = logger.ForContext<WeatherIngestService>();
     }
 
-    public async Task<int> FetchAndStoreAsync(CancellationToken cancellationToken)
+    /// <inheritdoc />
+    public Task<int> FetchAndStoreAsync(CancellationToken cancellationToken)
     {
-        IReadOnlyCollection<WeatherReading> readings = await this.externalWeatherClient.GetReadingsAsync(cancellationToken);
-        int writtenCount = await this.influxWriteService.WriteAsync(readings, cancellationToken);
-
-        this.logger.Information("Ingested {Count} readings", writtenCount);
-        return writtenCount;
+        throw new NotImplementedException();
     }
 }
